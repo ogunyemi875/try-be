dd4df80ddb9221b667a13ff0dd424f9af78f1f64
Ogunyemi Ibukunoluwa <70327188+ogunyemi875@users.noreply.github.com>
2023-07-12 13:37:31 +0100

    Add files via upload

---
+++ cproject.js
@@ -0,0 +1,16 @@
+
+//change heading
+document.getElementById("myH").innerHTML="Hello javscript";
+// function add(a,b){console.log(a+b)};
+// add(2,3);
+// function fiveTime(a){return (a)*5};
+// const b=fiveTime(6);
+// console.log(b);
+// function quadEqn(a,b){return(a**a+b**b)};
+// let c=quadEqn(2,3);
+// console.log(c);""
+const x= document.getElementByTagName("h2");
+x.innerHTML="gud marin";
+const y= document.getElementByTagName(p);
+y.innerHTML="okay";
+document.write(Date());
